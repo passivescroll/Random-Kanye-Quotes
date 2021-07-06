a2ac92fc983bffae02467bc126a7704d44d17a9a
sky <flahertysky12@gmail.com>
2021-07-06 14:00:32 -0400

    moving along

--- index.js
+++ index.js
@@ -1,61 +1,42 @@
 console.log('Welcome to Random Kanye Quotes!')
-
-const URL = ('https://api.kanye.rest')
+//API
 
 const quotePTag = document.querySelector('#quote-text')
 const refreshButton = document.querySelector('#Refresh-button')
 const favoriteButton = document.querySelector('#favorite-button')
 const favoriteContainer = document.querySelector('#liked-quotes')
 
+let currentQuote = {}
+
 function fetchKanye() {
-    return fetch(URL, {
-        headers: {
-            'Accept': 'applicaton/json'
-        } 
-    }).then(res => res.json())
-}  
+    return fetch('https://api.kanye.rest')
+    .then(resp => resp.json())
+    .then(data => {
+        currentQuote = data
+        const quote = data.quote 
+        quotePTag.innerHTML = quote
+        })
+    }
+
+refreshButton.addEventListener("click", fetchKanye);
+
+
+
+//need to create a function that will not display the the Kanye API
+
+//function displayKanye() {
+//  return fetch(URL, {
+//      headers: {
+//          'Accept': 'application/json'
+//      }
+//  })
+//
    
+//quotePTag.innerHTML 
 
+
+
+//parse into the ??body ??stringify 
    
-
-
-//function fetchKanye(){
-//    return fetch(url, {
-//        headers:{
-//            'Accept': 'application/json'
-//        }
-//    }).then(res => res.json())
-//      
-//    
-//}
-
-
-//function fetchKanyeQuote() {
-//    return fetch(URL, {
-//        headers: {
-//            'Accept': 'application/json'
-//        }
-//    })
-//    .then(res => res.json())
-//    .then(data => {
-//        const Quotes = data.Quotes
-//        quotePTag.innertext = Quotes
-//    })
-//}
-
-// function postKanyeQuote(body) {
-//     return fetch (URL, {
-//         method: 'POST',
-//         headers: {
-//             'Accept': 'applicaton/json'
-//         },
-//         body:JSON.stringify(body)
-//     })
-//     .then(res => res.json())
-    
-
-
-
-
 
 
