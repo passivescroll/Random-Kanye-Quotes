a14646f8ba902bd8acf9477a82aedf09d2375844
sky <flahertysky12@gmail.com>
2021-07-06 00:32:17 -0400

    Added to html

--- index.js
+++ index.js
@@ -1,9 +1,61 @@
-console.log('Welcome to Random Kanye Quotes!', 'color: orange')
+console.log('Welcome to Random Kanye Quotes!')
 
-const quotePtag = document.querySelector('#quote')
-const reloadButton = document.querySelector('#Refresh-button')
+const URL = ('https://api.kanye.rest')
 
-reloadButton.addEventListener('click', getKanyeQuote)
+const quotePTag = document.querySelector('#quote-text')
+const refreshButton = document.querySelector('#Refresh-button')
+const favoriteButton = document.querySelector('#favorite-button')
+const favoriteContainer = document.querySelector('#liked-quotes')
+
+function fetchKanye() {
+    return fetch(URL, {
+        headers: {
+            'Accept': 'applicaton/json'
+        } 
+    }).then(res => res.json())
+}  
+   
+
+   
+
+
+//function fetchKanye(){
+//    return fetch(url, {
+//        headers:{
+//            'Accept': 'application/json'
+//        }
+//    }).then(res => res.json())
+//      
+//    
+//}
+
+
+//function fetchKanyeQuote() {
+//    return fetch(URL, {
+//        headers: {
+//            'Accept': 'application/json'
+//        }
+//    })
+//    .then(res => res.json())
+//    .then(data => {
+//        const Quotes = data.Quotes
+//        quotePTag.innertext = Quotes
+//    })
+//}
+
+// function postKanyeQuote(body) {
+//     return fetch (URL, {
+//         method: 'POST',
+//         headers: {
+//             'Accept': 'applicaton/json'
+//         },
+//         body:JSON.stringify(body)
+//     })
+//     .then(res => res.json())
+    
+
+
+
 
 
 
---
+++ services.js
@@ -0,0 +1,37 @@
+
+
+  
+    
+
+ 
+
+
+
+
+
+
+//function fetchKanyeQuote() {
+//    return fetch(URL, {
+//        headers: {
+//            'Accept': 'application/json'
+//        }
+//    })
+//    .then(res => res.json())
+//    .then(data => {
+//        const Quotes = data.Quotes
+//        quotePTag.innertext = Quotes
+//    })
+//}
+
+// function postKanyeQuote(body) {
+//     return fetch (URL, {
+//         method: 'POST',
+//         headers: {
+//             'Accept': 'applicaton/json'
+//         },
+//         body:JSON.stringify(body)
+//     })
+//     .then(res => res.json())
+    
+
+
