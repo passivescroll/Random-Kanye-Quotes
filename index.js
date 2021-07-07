8217118fa129bfb6aaeef750c14bcdf028c11e7b
sky <flahertysky12@gmail.com>
2021-07-06 22:04:54 -0400

    main mvps are finished

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('Welcome to Random Kanye Quotes!')
 
-
+//Elements 
 const quotePTag = document.querySelector("#quote-text")
 const refreshButton = document.querySelector("#Refresh-button")
 const favoriteButton = document.querySelector("#favorite-button")
@@ -9,7 +9,7 @@ const favoriteContainer = document.querySelector("#myFavorites")
 let currentQuote = {}
 const favoriteQuote = []
 
-// create a function that will display the api 
+
 function fetchKanye() {
     return fetch('https://api.kanye.rest')
     .then(resp => resp.json())
@@ -20,13 +20,6 @@ function fetchKanye() {
         })
     }
 
-// add eventlistener to click event.
-refreshButton.addEventListener("click", fetchKanye);
-
-/// dont you just need to make a point to grab the one being showed. 
-
-       
-favoriteButton.addEventListener("click", saveQuote);
 
 function saveQuote() {
     if(!favoriteQuote.includes(currentQuote)) {
@@ -42,6 +35,13 @@ function saveQuote() {
         event.target.remove()
     }
 }   
+
+refreshButton.addEventListener("click", fetchKanye);
+      
+favoriteButton.addEventListener("click", saveQuote);
+
+
+
 
 //need to create a function that will not display the the Kanye API
 
