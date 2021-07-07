274f0294c1bc8e7254056da89ab09c6ff7ee97f1
sky <flahertysky12@gmail.com>
2021-07-06 21:52:21 -0400

    finished main mvp

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 console.log('Welcome to Random Kanye Quotes!')
-//API
 
-const quotePTag = document.querySelector('#quote-text')
-const refreshButton = document.querySelector('#Refresh-button')
-const favoriteButton = document.querySelector('#favorite-button')
-const favoriteContainer = document.querySelector('#liked-quotes')
+
+const quotePTag = document.querySelector("#quote-text")
+const refreshButton = document.querySelector("#Refresh-button")
+const favoriteButton = document.querySelector("#favorite-button")
+const favoriteContainer = document.querySelector("#myFavorites")
 
 let currentQuote = {}
+const favoriteQuote = []
 
+// create a function that will display the api 
 function fetchKanye() {
     return fetch('https://api.kanye.rest')
     .then(resp => resp.json())
@@ -18,9 +20,28 @@ function fetchKanye() {
         })
     }
 
+// add eventlistener to click event.
 refreshButton.addEventListener("click", fetchKanye);
 
+/// dont you just need to make a point to grab the one being showed. 
 
+       
+favoriteButton.addEventListener("click", saveQuote);
+
+function saveQuote() {
+    if(!favoriteQuote.includes(currentQuote)) {
+        favoriteQuote.push(currentQuote)
+        const li = document.createElement('li')
+        li.innerHTML = currentQuote.quote
+        favoriteContainer.append(li)
+        li.addEventListener('click', event => removequote(event))
+    }else {
+        alert('You already have that one')
+    }
+    function removequote(event) {
+        event.target.remove()
+    }
+}   
 
 //need to create a function that will not display the the Kanye API
 
